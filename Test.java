a3a4705d1351e4844ed43aaa5d552fc3d2436e45
Anastácia Canto <91332251+Anastacia-Canto@users.noreply.github.com>
2022-08-08 21:42:28 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,5 +2,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println(TenMinWalk.isValid(new char[] {'n', 'e', 's', 'w', 'n', 'e', 's', 'w', 'n', 's'}));
+		System.out.println(TenMinWalk.isValid(new char[] {'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}));
+		System.out.println(TenMinWalk.isValid(new char[] {'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's', 'w', 's'}));
+		System.out.println(TenMinWalk.isValid(new char[] {'n', 'e', 's', 'w', 'n', 'e', 's', 'w', 'n', 'e'}));
 	}
 }
